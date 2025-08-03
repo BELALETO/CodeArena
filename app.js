cc3843d80d7c0fcc9c55a32a2ba91158dcb225a3
Belal Sryo <belalsryo@gmail.com>
2025-08-03 21:21:21 +0300

    feat: add security and rate limiting middleware with necessary dependencies

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const dotenv = require('dotenv').config({ path: './config.env', quiet: true });
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const userRouter = require('./routes/userRouter');
 const problemRouter = require('./routes/problemRouter');
 const errorController = require('./controllers/errorController');
 const AppError = require('./utils/AppError');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const hpp = require('hpp');
 const app = express();
 
+const limiter = rateLimit({
+  limit: 100,
+  windowMs: 15 * 60 * 1000,
+  message:
+    'Too many requests from the same IP, wait 15 minutes untill you can request again.'
+});
+
 // middlewares
+app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
-
+app.use(mongoSanitize());
+app.use('/api', limiter);
+app.use(hpp());
 // testing middleware
 app.get('/', (req, res) => {
   res.status(200).json({
