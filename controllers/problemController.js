025e2553d926f31a7232c08e9cc510b1e80e1e86
Belal Sryo <belalsryo@gmail.com>
2025-08-12 23:06:08 +0300

    feat: implement pagination, sorting, and field selection for getProblems function

--- controllers/problemController.js
+++ controllers/problemController.js
@@ -3,7 +3,34 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/AppError');
 
 const getProblems = catchAsync(async (req, res, next) => {
-  const problems = await Problem.find();
+  const queryObj = { ...req.query };
+  const excludedFields = ['page', 'sort', 'limit', 'fields'];
+  excludedFields.forEach((el) => delete queryObj[el]);
+
+  let query = Problem.find(queryObj);
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+  if (req.query.fields) {
+    const fields = req.query.fields.split(',').join(' ');
+    query = query.select(fields);
+  } else {
+    query = query.select('-__v');
+  }
+  const page = req.query.page * 1 || 1;
+  const limit = req.query.limit * 1 || 100;
+  const skip = (page - 1) * limit;
+  query = query.skip(skip).limit(limit);
+  if (req.query.page) {
+    const numProblems = await Problem.countDocuments(queryObj);
+    if (skip >= numProblems) {
+      return next(new AppError(404, 'This page does not exist'));
+    }
+  }
+  const problems = await query;
   res.status(200).json({
     status: 'success',
     results: problems.length,
